f7b51f85cd3f83c34a84cf3ca57f954278266813
Chris P <ambassadorpirkl@gmail.com>
2026-02-10 16:14:37 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Note: Since index.html and App.jsx are in your root, 
-  // no additional 'root' or 'base' paths are needed.
-})
