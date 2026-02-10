b097e552ead5ed79921a6fe419f327b360e84ffc
Chris P <ambassadorpirkl@gmail.com>
2026-02-10 16:12:35 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-plugins: [react()],
+  plugins: [react()],
+  // Note: Since index.html and App.jsx are in your root, 
+  // no additional 'root' or 'base' paths are needed.
 })
